ba60a25e7be0c1dd7cb6f369fbfd3dec34016197
aastha21-appdevlper <143211046+aastha21-appdevlper@users.noreply.github.com>
2023-08-28 07:31:54 +0530

    Update streamlit_app.py
    updating error mistakes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-Streamlit.text('Omega 3 & Blueberry Oat Meal')
-Streamlit.text('Kale, Spinach & Rocket Smoothie')
-Streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oat Meal')
+streamlit.text('Kale, Spinach & Rocket SMoothie')
+streamlit.text('Hard-Boiled Free-Range Eggs')
